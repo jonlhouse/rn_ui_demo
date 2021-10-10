8853e34936864a8dddf1adbc72cf6446fc87926d
John House <jonlhouse@gmail.com>
2021-10-10 12:46:05 -0500

    babel config updated as per react-native-renanimated2 instructions

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    // Caution: Reanimated plugin has to be listed last. (https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/)
+    'react-native-reanimated/plugin',
+  ],
 };
